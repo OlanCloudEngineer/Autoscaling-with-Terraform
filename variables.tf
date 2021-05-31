08ab72fe0bfaf63817b515c9a79091630691970f
OlanCloudEngineer <olan.odun@gmail.com>
2021-05-31 13:03:32 +0100

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,21 @@
+
+variable "region" {
+  type = string
+  default = "us-east-2"
+}
+
+variable "image_id" {
+  type = string
+  default = "ami-922914f7"
+}
+
+variable "flavor" {
+  type = string
+  default = "t2.micro"
+}
+
+variable "ec2_instance_port" {
+  type = number
+  default = 80
+}
+
